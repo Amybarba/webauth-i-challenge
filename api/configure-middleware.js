761c2c0a021d474240f013d39eea77f29f517534
Amy Barba <Amybarba70@gmail.com>
2019-12-04 21:01:20 -0800

    coded config middleware file

---
+++ api/configure-middleware.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// This module exports a method that takes an express server 
+// as a parameter, and uses it to add various 3rd party middleware 
+// methods to it.
+// This is one method for helping your code to stay clean, but is not requried.
+
+module.exports = server => {
+    server.use(helmet());
+    server.use(express.json());
+    server.use(cors());
+};
