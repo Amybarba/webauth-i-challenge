c1625b51ca686380887e8559fb8f96605df63fb7
Amy Barba <Amybarba70@gmail.com>
2019-12-04 20:25:27 -0800

    coded server file

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// This project is modularized into routers. Here, we get a router object that
+// is exported from ./api-router.js, and bind it to the /api url.
+// In addition, a method is exported from ./configure-middleware.js, which takes
+// an express server object, and calls server.use() with a number of 3rd party
+// middleware providers. We could do the server.use() calls here... this just
+// makes the code a little cleaner. - Sean Kirby
+
+const apiRouter = require('./api-router.js');
+const configureMiddleware = require('./configure-middleware.js');
+
+const server = express();
+
+configureMiddleware(server);
+
+server.use('/api', apiRouter);
+
+module.exports = server;
